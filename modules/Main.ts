f1f41da56c1f1a9eade2a04833ff72c9c6562b7c
HappyRyan2 <HappyRyan2@Hotmail.com>
2025-05-05 15:55:13 -0700

    changed 5 rooms; added 1 room
    Changed rooms:
    - `two-wide-platforms`: added blocks in the corners
    - `the-tomb`: added an exit below so it generates more often
    - `gate-contraption`: slightly moved some paths and added an exit above
    - `tunnels`: added exits above and below
    - `empty-room`: made it slightly less empty

--- modules/Main.ts
+++ modules/Main.ts
@@ -49,8 +49,8 @@ world.tiles.set(-1, 0, "solid");
 // world.creatures.push(new Lizard(new Vector(225, 25), "left", 200, 3));
 
 export class Main {
-	static screen: World | RoomEditor = new LevelGenerator().generate();
-	// static screen: World | RoomEditor = new RoomEditor();
+	// static screen: World | RoomEditor = new LevelGenerator().generate();
+	static screen: World | RoomEditor = new RoomEditor();
 	// static screen: World | RoomEditor = world;
 
 	static update(canvasIO: CanvasIO) {
